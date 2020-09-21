98cee34fd2be35d5cec38f4f93d535bf8714430f
D.Sneha <dubeysneha26@gmail.com>
2020-09-21 20:42:39 +0530

    Modified Menu Items
    Modified Menu Items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,34 +13,19 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Inbox',
+      title: 'Check Availability',
       url: '/folder/Inbox',
-      icon: 'mail'
+      icon: 'warning'
     },
     {
-      title: 'Outbox',
+      title: 'Book a Slot',
       url: '/folder/Outbox',
-      icon: 'paper-plane'
-    },
-    {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart'
-    },
-    {
-      title: 'Archived',
-      url: '/folder/Archived',
       icon: 'archive'
     },
     {
-      title: 'Trash',
-      url: '/folder/Trash',
+      title: 'Cancel Slot',
+      url: '/folder/Favorites',
       icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
     }
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
